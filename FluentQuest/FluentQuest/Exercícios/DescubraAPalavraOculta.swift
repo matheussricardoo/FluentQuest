af06698d9753bbf027d77d6b20b65d3877e0af8a
Kleber-gadelha <145131511+Kleber-gadelha@users.noreply.github.com>
2025-03-21 17:54:02 -0300

    Atualizando o código
    Adicionando a linguagem em espanhol e francês.

--- FluentQuest/FluentQuest/Exercícios/DescubraAPalavraOculta.swift
+++ FluentQuest/FluentQuest/Exercícios/DescubraAPalavraOculta.swift
@@ -3,7 +3,7 @@ func iniciarDescubraAPalavraOculta(idioma: String) {
     if idioma == "Inglês" {
         var rodando = true
         
-        let englishWords: [String] = ["Ball", "Cat", "Dog"]
+        let englishWords: [String] = ["Dog", "Ball", "Love"]
         if let nomeAleatorio = englishWords.randomElement() {
             let randomWord = nomeAleatorio.count // Tamanho da palavra selecionada
             var palavraOculta: [Character] = Array(repeating: "_", count: randomWord) // Array de Character
@@ -28,6 +28,84 @@ func iniciarDescubraAPalavraOculta(idioma: String) {
                     
                     palavraOculta = palavraParcial
                     print(String(palavraOculta)) // Converte para String antes de imprimir
+                    
+                    if palavraOculta == vetorNomeAleatorio {
+                        print("Você acertou! A palavra era '\(nomeAleatorio)'!")
+                        rodando = false
+                    }
+                } else {
+                    print("Tente novamente. A palavra tem \(randomWord) letras!")
+                }
+            }
+        }
+    }
+    
+    else if idioma == "Espanhol" {
+        var rodando = true
+        
+        let englishWords: [String] = ["Perro", "Bola", "Amor"]
+        if let nomeAleatorio = englishWords.randomElement() {
+            let randomWord = nomeAleatorio.count
+            var palavraOculta: [Character] = Array(repeating: "_", count: randomWord)
+            let vetorNomeAleatorio = Array(nomeAleatorio.lowercased())
+            
+            print(String(palavraOculta))
+            
+            while rodando {
+                print("\nDigite aqui: ", terminator: "")
+                
+                if let inputUsuario = readLine()?.lowercased(), inputUsuario.count == randomWord {
+                    let vetorInput = Array(inputUsuario)
+                    var palavraParcial = palavraOculta
+                    
+                    for i in 0..<randomWord {
+                        if vetorInput[i] == vetorNomeAleatorio[i] {
+                            palavraParcial[i] = vetorInput[i]
+                        } else if vetorNomeAleatorio.contains(vetorInput[i]) {
+                            print("A letra '\(vetorInput[i])' existe, mas está na posição errada!")
+                        }
+                    }
+                    
+                    palavraOculta = palavraParcial
+                    print(String(palavraOculta))
+                    
+                    if palavraOculta == vetorNomeAleatorio {
+                        print("Você acertou! A palavra era '\(nomeAleatorio)'!")
+                        rodando = false
+                    }
+                } else {
+                    print("Tente novamente. A palavra tem \(randomWord) letras!")
+                }
+            }
+        }
+    } else{
+        var rodando = true
+        
+        let englishWords: [String] = ["Chien", "Balle", "Amour"]
+        if let nomeAleatorio = englishWords.randomElement() {
+            let randomWord = nomeAleatorio.count
+            var palavraOculta: [Character] = Array(repeating: "_", count: randomWord)
+            let vetorNomeAleatorio = Array(nomeAleatorio.lowercased())
+            
+            print(String(palavraOculta))
+            
+            while rodando {
+                print("\nDigite aqui: ", terminator: "")
+                
+                if let inputUsuario = readLine()?.lowercased(), inputUsuario.count == randomWord {
+                    let vetorInput = Array(inputUsuario)
+                    var palavraParcial = palavraOculta
+                    
+                    for i in 0..<randomWord {
+                        if vetorInput[i] == vetorNomeAleatorio[i] {
+                            palavraParcial[i] = vetorInput[i]
+                        } else if vetorNomeAleatorio.contains(vetorInput[i]) {
+                            print("A letra '\(vetorInput[i])' existe, mas está na posição errada!")
+                        }
+                    }
+                    
+                    palavraOculta = palavraParcial
+                    print(String(palavraOculta))
                     
                     if palavraOculta == vetorNomeAleatorio {
                         print("Você acertou! A palavra era '\(nomeAleatorio)'!")
