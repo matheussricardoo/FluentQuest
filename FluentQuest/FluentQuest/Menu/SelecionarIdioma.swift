cfcf30829515fb013dd413f064e100a21eaf75ff
Enzo Ferroni <125482399+EnzoFerroni@users.noreply.github.com>
2025-03-24 14:00:28 -0300

    alteração no

--- FluentQuest/FluentQuest/Menu/SelecionarIdioma.swift
+++ FluentQuest/FluentQuest/Menu/SelecionarIdioma.swift
@@ -1,21 +1,33 @@
-func selecionarIdioma() -> String? {
+var idiomaSelecionado: String? = nil
+let idiomaSalvo = idiomaSelecionado
+
+func selecionarIdioma() {
     print("""
-  Escolha seu idioma:
-  1 - Inglês
-  2 - Espanhol
-  3 - Francês
-  4 - Voltar
-  """)
-    
+    Escolha seu idioma:
+    1 - Inglês
+    2 - Espanhol
+    3 - Francês
+    4 - Voltar
+    """)
+
     while true {
         print("\nDigite o número: ", terminator: "")
         if let escolha = readLine(), let opcao = Int(escolha) {
             switch opcao {
-            case 1: return "Inglês"
-            case 2: return "Espanhol"
-            case 3: return "Francês"
-            case 4: return nil
-            default: print("Opção inválida. Digite 1 a 4")
+            case 1:
+                idiomaSelecionado = "Inglês"
+                return
+            case 2:
+                idiomaSelecionado = "Espanhol"
+                return
+            case 3:
+                idiomaSelecionado = "Francês"
+                return
+            case 4:
+                idiomaSelecionado = nil
+                return
+            default:
+                print("Opção inválida. Digite 1 a 4")
             }
         } else {
             print("Entrada inválida. Digite apenas números")
